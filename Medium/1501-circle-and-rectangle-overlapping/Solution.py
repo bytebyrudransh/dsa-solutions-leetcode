59e00a6087fdc8fb7457739b52812f674493cb5a
Rudransh <kashyaprudra22@gmail.com>
2026-09-19 18:21:34 +0530

    ✅ [Medium] #1501 - Circle and Rectangle Overlapping | Python | Runtime: 0 ms | Memory: 12.3 MB

---
+++ Medium/1501-circle-and-rectangle-overlapping/Solution.py
@@ -0,0 +1,11 @@
+import math
+class Solution(object):
+    def checkOverlap(self, radius, xCenter, yCenter, x1, y1, x2, y2):
+
+        closestX = max(x1, min(xCenter, x2))
+        closestY = max(y1, min(yCenter, y2))
+
+        distance = math.sqrt((closestX - xCenter) ** 2 + (closestY - yCenter) **2)
+
+        return distance <= radius
+        
