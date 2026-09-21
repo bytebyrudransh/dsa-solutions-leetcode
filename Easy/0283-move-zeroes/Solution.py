938f08b96bf6e7a7b4aee9f6f4cbb4dc6bdd5667
Rudransh <kashyaprudra22@gmail.com>
2026-09-21 20:04:54 +0530

    ✅ [Easy] #0283 - Move Zeroes | Python3 | Runtime: 773 ms | Memory: 20.5 MB

--- Easy/0283-move-zeroes/Solution.py
+++ Easy/0283-move-zeroes/Solution.py
@@ -1,9 +1,7 @@
 class Solution:
-    def moveZeroes(self, nums: list) -> None:
-        slow = 0
-        for fast in range(len(nums)):
-            if nums[fast] != 0 and nums[slow] == 0:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
+    def moveZeroes(self, nums: List[int]) -> None:
 
-            if nums[slow] != 0:
-                slow += 1
+        for i in nums:
+            if i==0:
+                nums.pop(nums.index(i))
+                nums.append(0)
