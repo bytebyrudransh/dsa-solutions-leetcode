d956815405bb147eb553f2e8454e5e39fc12b989
Rudransh <kashyaprudra22@gmail.com>
2026-09-22 14:56:19 +0530

    ✅ [Easy] #0969 - Number of Recent Calls | Python3 | Runtime: 40 ms | Memory: 24.5 MB

---
+++ Easy/0969-number-of-recent-calls/Solution.py
@@ -0,0 +1,17 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+        
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+
+        while self.q:
+            if self.q[0] < t - 3000:
+                self.q.popleft()
+            else:
+                break
+        
+        return len(self.q)
+
