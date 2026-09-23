14351604a863cd2bfa370362336b902117f18290
Rudransh <kashyaprudra22@gmail.com>
2026-09-23 19:02:22 +0530

    ✅ [Medium] #2413 - Smallest Number in Infinite Set | Python3 | Runtime: 17 ms | Memory: 20 MB

---
+++ Medium/2413-smallest-number-in-infinite-set/Solution.py
@@ -0,0 +1,20 @@
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.curr = 1
+        self.added = []
+        self.inHeap = set()
+
+    def popSmallest(self) -> int:
+        if self.added:
+            val = heapq.heappop(self.added)
+            self.inHeap.remove(val)
+            return val
+        val = self.curr
+        self.curr += 1
+        return val
+
+    def addBack(self, num: int) -> None:
+        if num < self.curr and num not in self.inHeap:
+            heapq.heappush(self.added,num)
+            self.inHeap.add(num)
