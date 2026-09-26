2803715dcccad5c3a7e9c8a010b3cc2642b65ad9
Rudransh <kashyaprudra22@gmail.com>
2026-09-26 18:32:18 +0530

    ✅ [Medium] #0012 - Integer to Roman | Python3 | Runtime: 4 ms | Memory: 19.2 MB

--- Medium/0012-integer-to-roman/Solution.py
+++ Medium/0012-integer-to-roman/Solution.py
@@ -1,16 +1,18 @@
-class Solution(object):
-    def intToRoman(self, num):
-        val_map = [
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        value_symbols = [
             (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
-            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
-            (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
+            (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
         ]
-
+        
         res = []
-        for val, sym in val_map:
+
+        for value, symbol in value_symbols:
             if num == 0:
                 break
-            cnt, num = divmod(num, val)
-            res.append(sym * cnt)
+            count = num // value
+            res.append(symbol * count)
+            num -= count * value
 
-        return "".join(res)
+        return ''.join(res)                
